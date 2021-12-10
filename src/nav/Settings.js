9ea79215cee2188b56c2304f2c3b35d10766833c
Brandon Swenson <bmswens@gmail.com>
2021-12-10 07:58:13 +0000

    change menu icon

--- src/nav/Settings.js
+++ src/nav/Settings.js
@@ -14,6 +14,7 @@ import SettingsIcon from '@mui/icons-material/Settings'
 import HelpIcon from '@mui/icons-material/Help'
 import StorageIcon from '@mui/icons-material/Storage'
 import InfoIcon from '@mui/icons-material/Info'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
 
 // custom
 import ClusterInfoDialog from '../dialogs/ClusterInfoDialog'
@@ -132,7 +133,7 @@ function Settings(props) {
                 onClick={openMenu}
                 aria-label="settings"
             >
-                <SettingsIcon
+                <MoreVertIcon
                     fontSize="large"
                 />
             </IconButton>
